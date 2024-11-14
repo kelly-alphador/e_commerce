4cb0a6982fd117983af530271792ad4d4928d241
kelly alphador <kellyalphador@gmail.com>
2024-11-14 22:31:45 +0100

    payement_enligne succes

--- e_commerce/Controllers/CommandeController.cs
+++ e_commerce/Controllers/CommandeController.cs
@@ -11,6 +11,7 @@ using iTextSharp.text;
 using iTextSharp.text.pdf;
 using System.IO;
 using iTextSharp.text.pdf.draw;
+using Humanizer;
 
 namespace e_commerce.Controllers
 {
@@ -56,107 +57,7 @@ namespace e_commerce.Controllers
         }
 
 
-        /*public ActionResult GenererPdfFacture(int idCommande)
-        {
-            using (var context = new E_COMMERCEEntities())
-            {
-                // Récupérer la commande par son ID
-                var command = context.COMMANDE.FirstOrDefault(c => c.id_com == idCommande);
-
-                if (command == null)
-                {
-                    return RedirectToAction("ErreurCommande"); // Redirection si la commande n'existe pas
-                }
-
-                // Récupérer les détails de commande à partir de DETAIL_COMMANDE
-                var produits = (from dc in context.DETAIL_COMMANDE
-                                join p in context.PRODUIT on dc.id_prod equals p.id_prod
-                                join u in context.USERS on command.id_users equals u.id_user
-                                where dc.id_com == command.id_com // Utiliser l'ID de la commande
-                                select new
-                                {
-                                    NomProduit = p.nom,
-                                    Quantite = dc.qte,
-                                    PrixUnitaire = dc.prix_unitaire,
-                                    Total = dc.qte * dc.prix_unitaire,
-                                    nomUser = u.nom,
-                                    telephone = u.telephone,
-                                    adresse = u.adresse,
-                                }).ToList();
-
-                if (produits.Count == 0)
-                {
-                    return RedirectToAction("ErreurCommande"); // Redirection si aucun produit trouvé
-                }
-
-                // Création du PDF
-                var document = new Document();
-                MemoryStream stream = new MemoryStream();
-                PdfWriter.GetInstance(document, stream).CloseStream = false;
-
-                document.Open();
-
-                // Titre centré
-                Paragraph titre = new Paragraph("Facture de Commande");
-                titre.Alignment = Element.ALIGN_CENTER;
-                document.Add(titre);
-
-                // Ajouter les informations de la commande
-                document.Add(new Paragraph("Commande ID : " + command.id_com));
-                document.Add(new Paragraph("Nom du Client : " + produits[0].nomUser));
-                document.Add(new Paragraph("Date : " + command.date_commande?.ToString("dd/MM/yyyy") ?? "Date non disponible"));
-                document.Add(new Paragraph("Telephone : " + produits[0].telephone));
-                document.Add(new Paragraph("Adresse : " + produits[0].adresse));
-
-                // Ajouter un espace avant la table
-                document.Add(new Paragraph("\n"));
-
-                // Créer la table des produits
-                PdfPTable table = new PdfPTable(4);
-                table.AddCell("Produit");
-                table.AddCell("Prix Unitaire");
-                table.AddCell("Quantité");
-                table.AddCell("Total");
-
-                foreach (var produit in produits)
-                {
-                    table.AddCell(produit.NomProduit);
-                    table.AddCell(produit.PrixUnitaire.ToString());
-                    table.AddCell(produit.Quantite.ToString());
-                    table.AddCell(produit.Total.ToString());
-                }
-
-                document.Add(table);
-
-                // Ajouter un autre espace si besoin
-                document.Add(new Paragraph("\n"));
-
-                var totalGeneral = (decimal)produits.Sum(p => p.Total);
-
-                // Conversion en euros
-                var totalEnEuro = ConvertirEnEuro(totalGeneral);
-
-                // Total général en Ariary aligné à droite
-                Paragraph totalAriary = new Paragraph("TOTAL : " + totalGeneral + " Ariary");
-                totalAriary.Alignment = Element.ALIGN_RIGHT;
-                document.Add(totalAriary);
-
-                // Total général en Euros aligné à droite
-                Paragraph totalEuro = new Paragraph("TOTAL (EUR) : " + totalEnEuro.ToString("F2") + " €");
-                totalEuro.Alignment = Element.ALIGN_RIGHT;
-                document.Add(totalEuro);
-                document.Close();
-
-                // Réinitialiser la position du flux mémoire
-                stream.Position = 0;
-
-                // Spécifier que le PDF doit être affiché dans le navigateur
-                Response.AppendHeader("Content-Disposition", "inline; filename=Facture_Commande_" + idCommande + ".pdf");
-
-                // Retourner le PDF en tant que flux
-                return File(stream, "application/pdf");
-            }
-        }*/
+     
         public ActionResult GenererPdfFacture(int idCommande)
         {
             using (var context = new E_COMMERCEEntities())
@@ -258,6 +159,11 @@ namespace e_commerce.Controllers
                 Paragraph totalAriary = new Paragraph("TOTAL : " + totalGeneral + " Ariary");
                 totalAriary.Alignment = Element.ALIGN_RIGHT;
                 document.Add(totalAriary);
+                //Afficher le chiffre en lettre
+                int chiffre_lettre = (int)totalGeneral;
+                Paragraph totalLettre = new Paragraph("TOTAL : " + chiffre_lettre.ToWords() + " Ariary");
+                totalLettre.Alignment = Element.ALIGN_CENTER;
+                document.Add(totalLettre);
 
                 document.Close();
 
@@ -364,7 +270,8 @@ namespace e_commerce.Controllers
         {
             // Taux de conversion fictif. Par exemple, 1 euro = 5000 Ariary
             decimal tauxConversion = 5000;
-            return montantAriary / tauxConversion;
+            decimal totalEuro=montantAriary / tauxConversion;
+            return totalEuro;
         }
 
         public JsonResult ViderPanier()
--- e_commerce/Controllers/PaymentsController.cs
+++ e_commerce/Controllers/PaymentsController.cs
@@ -1,9 +1,9 @@
 ﻿using e_commerce.configuration;
+using PayPal;
 using PayPal.Api;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace e_commerce.Controllers
@@ -17,9 +17,17 @@ namespace e_commerce.Controllers
         }
 
         // Action qui sera appelée lors du clic sur le bouton pour créer un paiement
+        // Méthode pour convertir le montant en Ariary en Euro
+
+        // Action qui gère la création du paiement avec PayPal
         [HttpPost]
-        public ActionResult CreatePayment()
+        public ActionResult CreatePayment(decimal montantAriary)
         {
+            decimal montantTotal = (montantAriary + 0.35m) / (1 - 0.029m);
+            // Conversion du montant en chaîne formatée avec deux décimales
+            string totalEnEuro = montantTotal.ToString("0.00", System.Globalization.CultureInfo.InvariantCulture);
+
+            // Configuration de PayPal
             var apiContext = PayPalConfiguration.GetAPIContext();
 
             var payment = new Payment
@@ -27,25 +35,48 @@ namespace e_commerce.Controllers
                 intent = "sale",
                 payer = new Payer { payment_method = "paypal" },
                 transactions = new List<Transaction>
+        {
+            new Transaction
+            {
+                amount = new Amount
                 {
-                    new Transaction
-                    {
-                        amount = new Amount { currency = "USD", total = "10.00" },
-                        description = "Achat de produit X"
-                    }
+                    currency = "EUR",
+                    total = totalEnEuro // Utilise le montant formaté
                 },
+                description = "Achat de produit X",
+                 payee = new Payee
+                 {
+                    email = "andriamampindrykelly@gmail.com" // Remplacez par l'e-mail du compte PayPal du vendeur
+                }
+            }
+        },
                 redirect_urls = new RedirectUrls
                 {
-                    return_url = "http://votresite.com/Payments/PaymentSuccess",
-                    cancel_url = "http://votresite.com/Payments/PaymentCancel"
+                    return_url = "https://localhost:44331/Payments/PaymentSuccess",
+                    cancel_url = "https://localhost:44331/Payments/PaymentCancel"
                 }
             };
 
-            var createdPayment = payment.Create(apiContext);
-            var approvalUrl = createdPayment.links.FirstOrDefault(x => x.rel.Equals("approval_url", StringComparison.OrdinalIgnoreCase)).href;
+            try
+            {
+                // Création du paiement avec PayPal
+                var createdPayment = payment.Create(apiContext);
 
-            return Redirect(approvalUrl);
+                // Récupérer l'URL d'approbation
+                var approvalUrl = createdPayment.links
+                    .FirstOrDefault(x => x.rel.Equals("approval_url", StringComparison.OrdinalIgnoreCase))
+                    .href;
+
+                return Redirect(approvalUrl);
+            }
+            catch (PaymentsException ex)
+            {
+                // Gérer l'exception et afficher un message d'erreur
+                ViewBag.ErrorMessage = "Erreur de création du paiement PayPal : " + ex.Message;
+                return View("Error"); // Vue pour afficher les erreurs
+            }
         }
+
 
         // Action appelée lorsque le paiement est validé
         public ActionResult PaymentSuccess(string paymentId, string token, string PayerID)
@@ -59,10 +90,15 @@ namespace e_commerce.Controllers
                 return View("FailureView");
             }
 
-            // Mettre à jour la commande et marquer comme payée dans la base de données
-            // Envoyer une facture ou confirmation à l'utilisateur
+            // Ajouter ici la logique de mise à jour de la commande et d'envoi de facture
 
-            return View("SuccessView");
+            ViewBag.PaymentId = executedPayment.id;
+            ViewBag.PayerEmail = executedPayment.payer.payer_info.email;
+            ViewBag.TransactionId = executedPayment.transactions[0].related_resources[0].sale.id;
+            ViewBag.Amount = executedPayment.transactions[0].amount.total;
+            ViewBag.Currency = executedPayment.transactions[0].amount.currency;
+
+            return View("PaymentSuccess");
         }
 
         // Action appelée si l'utilisateur annule le paiement
